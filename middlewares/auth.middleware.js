fb6cf4ee765a7a542acba7b826fe43684ab96476
samuelikohn <samuelikohn@gmail.com>
2026-05-03 19:18:43 -0500

    added arcjet rate limiting

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -3,35 +3,38 @@ import { JWT_SECRET } from "../config/env.js"
 import User from "../models/user.model.js"
 
 const authorize = async (req, res, next) => {
-    try {
-        let token 
-        if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
-            token = req.headers.authorization.split(" ")[1]
-        }
+	try {
+		let token
+		if (
+			req.headers.authorization &&
+			req.headers.authorization.startsWith("Bearer")
+		) {
+			token = req.headers.authorization.split(" ")[1]
+		}
 
-        if (!token) {
-            return res.status(401).json({
-                message: "Unauthorized",
-                error: error.message
-            })
-        }
-        const decoded = jwt.verify(token, JWT_SECRET)
-        const user = await User.findById(decoded.UserID)
-        if (!user) {
-            return res.status(401).json({
-                message: "Unauthorized",
-                error: error.message
-            })
-        }
+		if (!token) {
+			return res.status(401).json({
+				message: "Unauthorized",
+				error: "No authorization token found"
+			})
+		}
+		const decoded = jwt.verify(token, JWT_SECRET)
+		const user = await User.findById(decoded.UserID)
+		if (!user) {
+			return res.status(401).json({
+				message: "Unauthorized",
+				error: "Unable to verify user ID"
+			})
+		}
 
-        req.user = user
-        next()
-    } catch (error) {
-        res.status(401).json({
-            message: "Unauthorized",
-            error: error.message
-        })
-    }
+		req.user = user
+		next()
+	} catch (error) {
+		res.status(401).json({
+			message: "Unauthorized",
+			error: error.message
+		})
+	}
 }
 
 export default authorize
