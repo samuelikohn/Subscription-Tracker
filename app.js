c9cc09fbf3a5653e345d6dfa28a9d6a8467330cd
samuelikohn <samuelikohn@gmail.com>
2026-06-03 18:31:23 -0500

    removed arcjet middleware

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ import authRouter from "./routes/auth.routes.js"
 import subscriptionRouter from "./routes/subscription.routes.js"
 import connectToDatabase from "./database/mongodb.js"
 import errorMiddleware from "./middlewares/error.middleware.js"
-import arcjetMiddleware from "./middlewares/arcjet.middleware.js"
 import workflowRouter from "./routes/workflow.routes.js"
 
 const app = express()
@@ -15,7 +14,6 @@ app.use(errorMiddleware)
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
-app.use(arcjetMiddleware)
 
 app.use("/api/v1/auth", authRouter)
 app.use("/api/v1/users", userRouter)
